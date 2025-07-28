3a0a199ec7e7267c8f020a37f52b1c50a3a90c45
bearweny <3206441974@qq.com>
2025-07-28 13:30:30 +0800

    modify2

--- src/main/java/com/lsy/MusicApp.java
+++ src/main/java/com/lsy/MusicApp.java
@@ -12,14 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @MapperScan("com.lsy.mapper")
 public class MusicApp {
    @GetMapping("/hello")
-public String hello(){
+   public String hello(){
        return "hello spring boot";
-}
+   }
 
-public static void main(String[] args) {
+   public static void main(String[] args) {
     SpringApplication.run(MusicApp.class);
     System.out.println("hello spring boot1");
     System.out.println("hello spring boot2");
     System.out.println("hello spring boot3");
-}
+   }
 }

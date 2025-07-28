147cd8e4f927b9c37107a91555f1cac2a3aede20
bearweny <3206441974@qq.com>
2025-07-28 13:23:22 +0800

    modify

--- src/main/java/com/lsy/MusicApp.java
+++ src/main/java/com/lsy/MusicApp.java
@@ -4,10 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Scanner;
 
 @SpringBootApplication
 @RestController
@@ -15,11 +13,13 @@ import java.util.Scanner;
 public class MusicApp {
    @GetMapping("/hello")
 public String hello(){
-    return "hello spring boot";
+       return "hello spring boot";
 }
 
-public static void main(String[] args)
-{
-SpringApplication.run(MusicApp.class);
+public static void main(String[] args) {
+    SpringApplication.run(MusicApp.class);
+    System.out.println("hello spring boot1");
+    System.out.println("hello spring boot2");
+    System.out.println("hello spring boot3");
 }
 }
